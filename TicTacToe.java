63a6b3dfab44a70bcc6930434513c1788ac0d63c
James Kelly <jek030@bucknell.edu>
2019-12-29 18:34:02 -0500

    added comments and initializer

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,11 +4,18 @@ import java.util.Scanner;
 
 
 public class TicTacToe {
-    //TODO: make init function
-    private String[][] board = new String[3][3];
-    private String x = "X";
-    private String o = "O";
-    private boolean is_X_Turn = true; //always start as true
+    private String[][] board;
+    private String x;
+    private String o;
+    private boolean is_X_Turn;
+
+    //init
+    public TicTacToe() {
+        board =  new String[3][3];
+        x = "X";
+        o = "O"; //O not 0, o not zero
+        is_X_Turn = true;
+    }
     
 
     public static void main(String[] args) {
@@ -38,6 +45,9 @@ public class TicTacToe {
         }
     }
 
+    /**
+     * Change the current player's turn.
+     */
     private void changeTurn() {
         if (is_X_Turn) {
             is_X_Turn = false;
@@ -58,23 +68,25 @@ public class TicTacToe {
             System.out.println("\t---------"); 
             
     }
-
+    
     private boolean isWinner() {
         //check if board contains a winner, return true, else false
         //aslo check if it is a tie
         return false;
     }
-    
+
+    /**
+     * Print who's turn it is.
+     */
     private void printTurn() {
-        if(is_X_Turn) 
-            {
+        if(is_X_Turn) {
                 System.out.println(x + "'s turn. Enter a valid number.");
-                
-            } else 
-            {
+            } else {
                 System.out.println(o + "'s turn. Enter a valid number.");
             }
     }
+
+
     private void play() {
         //MAIN GAME LOOP
         while(!isWinner()){
@@ -85,17 +97,18 @@ public class TicTacToe {
             try {
                 int in = scanner.nextInt();
                 if (in >= 1 && in <=9){
-
-                    while (( board[(in-1)/3][(in-1)%3].equals("X") || board[(in-1)/3][(in-1)%3].equals("O"))) {
+                    //maybe not the correct while loop, while (true) ... before all the in checking
+                    //
+                    while (( board[(in-1)/3][(in-1)%3].equals(x) || board[(in-1)/3][(in-1)%3].equals(o))) {
                         System.out.println("INVALID INPUT: enter a space that has not been taken.");
                         in = scanner.nextInt();
 
                     }
 
                     if(is_X_Turn){
-                        board[(in-1)/3][(in-1)%3] = "X";
+                        board[(in-1)/3][(in-1)%3] = x;
                     } else {
-                        board[(in-1)/3][(in-1)%3] = "0";
+                        board[(in-1)/3][(in-1)%3] = o;
                     }
                     
 
@@ -118,6 +131,7 @@ public class TicTacToe {
             
             
         }
+        
     }
 
 
